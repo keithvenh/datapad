e3220457dab609b874e4587cfa257230d9e10080
Keith VenHuizen <keith.venhuizen@gmail.com>
2020-12-10 16:22:32 -1000

    Add User Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+    username: "Keith",
+    email: "keith@datapad.io",
+    password: "lukecage2",
+    credits: 0,
+)
+
+User.create!(
+    username: "Zack",
+    email: "zack@datapad.io",
+    password: "lukecage2",
+    credits: 0,
+)
+
+User.create!(
+    username: "GameMaster",
+    email: "gamemaster@datapad.io",
+    password: "lukecage2",
+    credits: 0,
+)
