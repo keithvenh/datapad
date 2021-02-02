d585e887ab9e53cb4bb8a00fba857f7f8f7d282e
Keith VenHuizen <keith.venhuizen@gmail.com>
2021-02-01 14:53:07 -1000

    Add Home Page Styling

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,11 @@
 class Character < ApplicationRecord
+
     has_many :character_skills, dependent: :delete_all
+    has_many :skills, through: :character_skills
+
     has_one :character_career, dependent: :delete
+    has_one :career, through: :character_career
+
     has_many :character_specializations, dependent: :delete_all
+    has_many :specializations, through: :character_specializations
 end
