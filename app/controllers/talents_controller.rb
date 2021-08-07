4a53614777a83cda87727b68619bb8c342da15b5
Keith VenHuizen <keith.venhuizen@gmail.com>
2021-08-06 22:31:26 -1000

    Add Edit action for Talents

--- app/controllers/talents_controller.rb
+++ app/controllers/talents_controller.rb
@@ -1,4 +1,29 @@
 class TalentsController < ApplicationController
-  def edit
+  
+  layout 'admin'
+
+  def index
+    @talents = Talent.all.order(:name)
   end
+
+  def edit
+    @talent = Talent.find(params[:id])
+  end
+
+  def update
+    @talent = Talent.find(params[:id])
+    if @talent.update(talent_params)
+      flash[:notice] = "You have successfully updated #{@talent.name}."
+      redirect_to talents_path
+    else
+      flash.now[:alert] = "Beep Boop. Something went wrong."
+      render 'edit'
+    end
+  end
+
+  private
+
+    def talent_params
+      params.require(:talent).permit(:name, :active, :activation_type, :shortDesc, :longDesc, :force, :ranked, :book_id)
+    end
 end
